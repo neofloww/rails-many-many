462d2dd5dd3e4c8680f4995710e4ee851522de1a
Wendell Wolfe <wendell.wolfe@cerner.com>
2016-12-02 22:28:53 -0600

    habm

---
+++ blog/db/seeds.rb
@@ -0,0 +1,36 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+
+assemblies = [
+  { name: 'Airplane' },
+  { name: 'Boat' }
+]
+
+assemblies.each do |r|
+  if Assembly.exists? name: r[:name]
+    puts "Exists #{r}"
+  else
+    puts "Adding #{r}"
+    Assembly.create(r)
+  end
+end
+
+parts = [
+  { part_number: '3YUS', assembly_ids: ['1'] },
+  { part_number: '8ATE' }
+]
+
+parts.each do |r|
+  if Part.exists? part_number: r[:part_number]
+    puts "Exists #{r}"
+  else
+    puts "Adding #{r}"
+    Part.create(r)
+  end
+end
